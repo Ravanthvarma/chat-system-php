56979000fce78d18ffb0ee3bd6ba365266351fdf
Ravanthvarma <67589419+Ravanthvarma@users.noreply.github.com>
2022-04-09 17:15:41 +0530

    adding files
    submitting

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php include_once "header.php"; ?>
+<body>
+    <div class="wrapper">
+   <section class="form login">
+       <header>Chat App</header>
+       <form action="#" autocomplete = "off">
+           <div class="error-txt"></div>
+           <div class="field input">
+            <label>Email Address</label>
+            <input type="text" name="email" placeholder="Email Address">
+        </div>
+        <div class="field input">
+            <label>Password</label>
+            <input type="password" name="password" placeholder="Entre your Password">
+            <i class="fa fa-eye"></i>
+        </div>
+        <div class="field button">
+            <input type="submit" value="continue to chat">
+        </div>
+       </form>
+
+       <div class="link">Not yet signed up?<a href="index.php">Signup Now</a></div>
+   </section>
+    </div>
+    <script src="javascript/javascript1.js">
+
+    </script>
+    <script src="javascript/login.js"></script>
+</body>
+</html>
